b713c6d5d17e4c723f0fce20e8ad914e42e61c7d
Alex <luongalex06@gmail.com>
2024-09-09 13:20:46 -0600

    i finished up my work

--- text_utils.py
+++ text_utils.py
@@ -1,14 +1,35 @@
 def count_chars(text):
-    """
-    Count the number of characters in a text
-    """
+    if not text:
+        return 0
+    
+    # if the text doesnt have anything, it returns 0
+    
+    total = 0
+    for chars in text:
+        total += 1
+    return total
+
+    # it counts all of the characters that are in the text and adds it to the total
 
 def count_words(text):
-    """
-    Count the number of words in a text
-    """
+    if not text:
+        return 0
+    
+    # if the text doesnt have anything, it returns 0
+
+    total = 0
+    for words in text.split(" "):
+        total += 1
+    return total
+
+    # it counts all of the words (every word is split by a space " ") that are in the text and adds it to the total
+
 
 def count_sentences(text):
-    """
-    Count the number of sentences in a text
-    """
+    total = 0
+    for sentences in text:
+        if sentences == "." or sentences == "?" or sentences == "!":
+            total += 1
+    return total
+
+    # it counts all of the punctuation in the text and adds them up together
